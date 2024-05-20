a2080eca96e97d6741d8c5366621d46c89cf44bb
Sawyer Powell <sawyerhpowell@gmail.com>
2024-05-20 15:49:33 -0700

    First draft of lisp machines article

--- org/publish.el
+++ org/publish.el
@@ -157,6 +157,8 @@
    "</div></div>"
    "</body>\n"
    "<script>hljs.highlightAll();</script>\n" ; Does syntax highlighting on code blocks
+   "<script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>"
+   "<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>"
    "</html>"))
 
 (defun sawyer-html-build-head (info)
